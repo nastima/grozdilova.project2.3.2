2f0a02fcaf7118b3473aababd79968fb4f56c1fa
Grozdilova A <grozdilova@mail.ru>
2025-10-10 15:44:42 +0300

    настройка Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello React + TypeScript + Vite!</h1>
+            <p>Your setup is working! 🎉</p>
+        </div>
+    )
+}
+
+export default App
